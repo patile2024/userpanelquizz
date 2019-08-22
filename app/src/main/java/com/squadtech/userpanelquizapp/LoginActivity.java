14cfa93ad3975cc294bec41ddc350c405e1c6291
shahrukhktk <shahrukhktk760@gmail.com>
2019-08-22 13:43:23 +0500

    start - commit

--- app/src/main/java/com/squadtech/userpanelquizapp/LoginActivity.java
+++ app/src/main/java/com/squadtech/userpanelquizapp/LoginActivity.java
@@ -6,11 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,7 +25,7 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button LoginBtn;
+    private ImageButton LoginBtn;
     private EditText eEmail, ePass;
     private TextView txtNoAcount;
     private FirebaseAuth mAuth;
@@ -37,7 +39,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         mProgress = new ProgressDialog(this);
-        LoginBtn = (Button)findViewById(R.id.LoginBtn);
+        LoginBtn = (ImageButton)findViewById(R.id.LoginBtn);
         eEmail = (EditText)findViewById(R.id.LoginEmail);
         ePass = (EditText)findViewById(R.id.LoginPass);
         txtNoAcount = (TextView) findViewById(R.id.txtNoAccount);
