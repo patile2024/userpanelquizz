51db023cadd3e7e800d6a3bbf83145cf48b27984
shahrukhktk <shahrukhktk760@gmail.com>
2019-08-22 12:12:55 +0500

    start - commit

---
+++ app/src/main/java/com/squadtech/userpanelquizapp/SplashScreen.java
@@ -0,0 +1,54 @@
+package com.squadtech.userpanelquizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.github.ybq.android.spinkit.sprite.Sprite;
+import com.github.ybq.android.spinkit.style.FoldingCube;
+
+public class SplashScreen extends AppCompatActivity
+{
+    ProgressBar progress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        progress = (ProgressBar) findViewById(R.id.progress_bar_id);
+        Sprite foldingCube = new FoldingCube();
+        progress.setIndeterminateDrawable(foldingCube);
+
+        Thread thread = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                super.run();
+                for (int i=0; i<100;)
+                {
+                    try
+                    {
+                        sleep(300);
+                    }
+                    catch (Exception ex)
+                    {
+                        ex.printStackTrace();
+                    }
+                    progress.setProgress(0);
+                    i = i+10;
+
+                }
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+
+            }
+        };
+        thread.start();
+
+    }
+}
