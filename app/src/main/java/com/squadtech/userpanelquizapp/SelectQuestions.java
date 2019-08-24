aaa6f6fee09949c3e7f26943ffd50a9d184bf3f9
ali-shahkust <syed.ali.shah3938@gmail.com>
2019-08-24 12:50:07 +0500

    22 aug

---
+++ app/src/main/java/com/squadtech/userpanelquizapp/SelectQuestions.java
@@ -0,0 +1,14 @@
+package com.squadtech.userpanelquizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectQuestions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_questions);
+    }
+}
