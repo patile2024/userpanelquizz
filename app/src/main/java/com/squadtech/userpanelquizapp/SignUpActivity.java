a6a44241f75e9c931e502971d2174670bb791d7d
shahrukhktk <shahrukhktk760@gmail.com>
2019-09-04 15:10:40 +0500

    SECOND

--- app/src/main/java/com/squadtech/userpanelquizapp/SignUpActivity.java
+++ app/src/main/java/com/squadtech/userpanelquizapp/SignUpActivity.java
@@ -22,6 +22,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -97,12 +98,7 @@ public class SignUpActivity extends AppCompatActivity {
 
                 if (task.isSuccessful()){
 
-                    String SaveCurrentDate;
-
-                    Calendar calendar = Calendar.getInstance();
-                    SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
-                    SaveCurrentDate = currentDate.format(calendar.getTime());
-
+                    String date = DateFormat.getDateInstance().format(new Date());
 
                     mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(FirebaseAuth.getInstance().getUid());
                     final HashMap<String , Object> userMap = new HashMap<>();
@@ -112,7 +108,7 @@ public class SignUpActivity extends AppCompatActivity {
                     userMap.put("user_pass" , sPass);
                     userMap.put("user_uid", FirebaseAuth.getInstance().getUid());
                     userMap.put("user_dp" , "default");
-                    userMap.put("registered_date",SaveCurrentDate );
+                    userMap.put("registered_date",date );
                     userMap.put("total_points" ,"10");
 
                     mProgress.dismiss();
